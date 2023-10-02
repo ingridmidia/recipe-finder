e6b7c96e1334851d55e6abd124e3916f916330ac
Liz FIschenich <Liz.Devlin85@gmail.com>
2023-10-02 14:19:33 -0500

    added js file. started code for api call for food

--- script.js
+++ script.js
@@ -4,8 +4,26 @@
 
 //api call: use user input to make api call
   //Filter by main ingredient-food
-www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast
+//www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast
         //clckable picture from main ingrediets to 
  //Filter by main ingredient-alcohol Search by ingredient
-www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin
+//www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin
 
+var userInputFood = "banana"
+var userInputDrink = "gin"
+
+var API_FoodURL = "https://cors-anywhere.herokuapp.com/" + "www.themealdb.com/api/json/v1/1/filter.php?i=" + userInputFood;
+ console.log(API_FoodURL);
+
+ fetch(API_FoodURL)
+.then(function (response){ 
+    return response.json();
+    })
+.then(function (data){
+    console.log(data);
+})
+for (var i = 0; i < data.length; i++) {
+    console.log(data[0].strMeal, data[0].idMeal);
+    
+}
+var API_DrinkURL = "www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + userInputDrink;
