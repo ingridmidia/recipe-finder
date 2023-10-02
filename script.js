137861ca30579a7af5b4376344dd816c2001032b
Liz FIschenich <Liz.Devlin85@gmail.com>
2023-10-02 12:03:41 -0500

    saving javascrip psudocode

---
+++ script.js
@@ -0,0 +1,11 @@
+//add event listeners: grab user input
+    //main ingredient Food
+    //main ingredient alcohol
+
+//api call: use user input to make api call
+  //Filter by main ingredient-food
+www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast
+        //clckable picture from main ingrediets to 
+ //Filter by main ingredient-alcohol Search by ingredient
+www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin
+
