8d4ae00ff0cb9c1c438a13692e82fade1c3e9f88
ingridmidia <ingridmidia@gmail.com>
2023-10-05 10:58:19 -0500

    show full recipe

--- assets/js/drink-results.js
+++ assets/js/drink-results.js
@@ -1,5 +1,6 @@
 var alcohol = document.location.search.split("=")[1];
 var API_DrinkURL = "https://cors-anywhere.herokuapp.com/www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + alcohol;
+var ingredientsArray = [];
 
 fetch(API_DrinkURL)
     .then(function (response) {
@@ -10,21 +11,55 @@ fetch(API_DrinkURL)
     })
     .then(function (data) {
         console.log(data);
-        if (data.drinks.length < 5 || data.drinks.length < 4) {
-            for (var i = 0; i < 3; i++) { // error if i is less than 5
-                console.log(data.drinks[i].strMeal);
-                var results = document.getElementById("drink-results");
-                var result = document.createElement("button");
-                result.textContent = data.drinks[i].strDrink;
-                results.appendChild(result);
-            }
-        } else {
-            for (var i = 0; i < 5; i++) {
-                var results = document.getElementById("drink-results");
-                var result = document.createElement("button");
-                result.textContent = data.drinks[i].strDrink;
-                results.appendChild(result);
-            }
+
+        for (var i = 0; i < data.drinks.length; i++) { // error if i is less than 5
+            console.log(data.drinks[i].strDrink);
+            var results = document.getElementById("drink-results");
+            var result = document.createElement("button");
+            result.textContent = data.drinks[i].strDrink;
+            results.appendChild(result);
+
+            result.addEventListener("click", function (event) {
+                var recipeName = event.target.innerText;
+                console.log(recipeName);
+
+                var API_RecipeURL = "https://cors-anywhere.herokuapp.com/www.thecocktaildb.com/api/json/v1/1/search.php?s=" + recipeName;
+                console.log(API_RecipeURL);
+
+                fetch(API_RecipeURL)
+                    .then(function (response) {
+                        if (!response.ok) {// validates input
+                            throw response.json();
+                        }
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        console.log(data);
+                        for (var i = 1; i <= 15; i++) {
+                            var ingredientKey = `strIngredient${i}`;
+                            var measurementKey = `strMeasure${i}`;
+                            console.log(data.drinks[0][ingredientKey]);
+                            if (data.drinks[0][ingredientKey] && data.drinks[0][ingredientKey].trim() !== "") {
+                                if (data.drinks[0][measurementKey] && data.drinks[0][measurementKey].trim() !== "") {
+                                    ingredientsArray.push({ ingredient: data.drinks[0][ingredientKey], measurement: data.drinks[0][measurementKey] })
+                                }
+                            }
+                        }
+                        var recipeCardEl = document.getElementById("recipeCard");
+                        var drinkTitleEl = document.createElement("ul");
+                        recipeCardEl.appendChild(drinkTitleEl);
+                        drinkTitleEl.textContent = data.drinks[0].strDrink;
+                        for (var i = 0; i < ingredientsArray.length; i++) {
+                            var ingredientEl = document.createElement("li")
+                            ingredientEl.textContent = ingredientsArray[i].measurement + " " + ingredientsArray[i].ingredient;
+                            recipeCardEl.appendChild(ingredientEl);
+                        }
+                        var instructionsTitle = document.createElement("p");
+                        instructionsTitle.textContent = "Instructions";
+                        recipeCardEl.appendChild(instructionsTitle);
+                        instructionsTitle.textContent = data.drinks[0].strInstructions;
+                    })
+            })
         }
     })
     .catch(function () {
