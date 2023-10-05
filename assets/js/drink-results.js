fdd32dfb4b5f5788e3ee5456cdd8ebc016d78375
ingridmidia <ingridmidia@gmail.com>
2023-10-05 12:55:00 -0500

    limited amount of recipes shown

--- assets/js/drink-results.js
+++ assets/js/drink-results.js
@@ -11,56 +11,14 @@ fetch(API_DrinkURL)
     })
     .then(function (data) {
         console.log(data);
-
-        for (var i = 0; i < data.drinks.length; i++) { // error if i is less than 5
-            console.log(data.drinks[i].strDrink);
-            var results = document.getElementById("drink-results");
-            var result = document.createElement("button");
-            result.textContent = data.drinks[i].strDrink;
-            results.appendChild(result);
-
-            result.addEventListener("click", function (event) {
-                var recipeName = event.target.innerText;
-                console.log(recipeName);
-
-                var API_RecipeURL = "https://cors-anywhere.herokuapp.com/www.thecocktaildb.com/api/json/v1/1/search.php?s=" + recipeName;
-                console.log(API_RecipeURL);
-
-                fetch(API_RecipeURL)
-                    .then(function (response) {
-                        if (!response.ok) {// validates input
-                            throw response.json();
-                        }
-                        return response.json();
-                    })
-                    .then(function (data) {
-                        console.log(data);
-                        for (var i = 1; i <= 15; i++) {
-                            var ingredientKey = `strIngredient${i}`;
-                            var measurementKey = `strMeasure${i}`;
-                            console.log(data.drinks[0][ingredientKey]);
-                            if (data.drinks[0][ingredientKey] && data.drinks[0][ingredientKey].trim() !== "") {
-                                if (data.drinks[0][measurementKey] && data.drinks[0][measurementKey].trim() !== "") {
-                                    ingredientsArray.push({ ingredient: data.drinks[0][ingredientKey], measurement: data.drinks[0][measurementKey] })
-                                }
-                            }
-                        }
-                        var recipeCardEl = document.getElementById("recipeCard");
-                        var drinkTitleEl = document.createElement("ul");
-                        recipeCardEl.appendChild(drinkTitleEl);
-                        drinkTitleEl.textContent = data.drinks[0].strDrink;
-                        for (var i = 0; i < ingredientsArray.length; i++) {
-                            var ingredientEl = document.createElement("li")
-                            ingredientEl.textContent = ingredientsArray[i].measurement + " " + ingredientsArray[i].ingredient;
-                            recipeCardEl.appendChild(ingredientEl);
-                        }
-                        var instructionsTitle = document.createElement("p");
-                        instructionsTitle.textContent = "Instructions";
-                        recipeCardEl.appendChild(instructionsTitle);
-                        instructionsTitle.textContent = data.drinks[0].strInstructions;
-                    })
-            })
+        if (data.drinks.length < 5) {
+            data = data.drinks;
+            showRecipes(data);
+        } else {
+            data = data.drinks.slice(0, 5);
+            showRecipes(data);
         }
+
     })
     .catch(function () {
         var results = document.getElementById("drink-results");
@@ -68,3 +26,55 @@ fetch(API_DrinkURL)
         errorMessage.textContent = "Sorry, no recipes found with this ingredient!"; // TODO: bigger font size and red
         results.appendChild(errorMessage);
     })
+
+function showRecipes(data) {
+    for (var i = 0; i < data.length; i++) {
+        var results = document.getElementById("drink-results");
+        var result = document.createElement("button");
+        result.textContent = data[i].strDrink;
+        results.appendChild(result);
+
+        result.addEventListener("click", function (event) {
+            var recipeName = event.target.innerText;
+            console.log(recipeName);
+
+            var API_RecipeURL = "https://cors-anywhere.herokuapp.com/www.thecocktaildb.com/api/json/v1/1/search.php?s=" + recipeName;
+            console.log(API_RecipeURL);
+
+            fetch(API_RecipeURL)
+                .then(function (response) {
+                    if (!response.ok) {// validates input
+                        throw response.json();
+                    }
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                    for (var i = 1; i <= 15; i++) {
+                        var ingredientKey = `strIngredient${i}`;
+                        var measurementKey = `strMeasure${i}`;
+                        console.log(data.drinks[0][ingredientKey]);
+                        if (data.drinks[0][ingredientKey] && data.drinks[0][ingredientKey].trim() !== "") {
+                            if (data.drinks[0][measurementKey] && data.drinks[0][measurementKey].trim() !== "") {
+                                ingredientsArray.push({ ingredient: data.drinks[0][ingredientKey], measurement: data.drinks[0][measurementKey] })
+                            }
+                        }
+                    }
+                    var recipeCardEl = document.getElementById("recipeCard");
+                    recipeCardEl.innerHTML = "";
+                    var drinkTitleEl = document.createElement("ul");
+                    recipeCardEl.appendChild(drinkTitleEl);
+                    drinkTitleEl.textContent = data.drinks[0].strDrink;
+                    for (var i = 0; i < ingredientsArray.length; i++) {
+                        var ingredientEl = document.createElement("li")
+                        ingredientEl.textContent = ingredientsArray[i].measurement + " " + ingredientsArray[i].ingredient;
+                        recipeCardEl.appendChild(ingredientEl);
+                    }
+                    var instructionsTitle = document.createElement("p");
+                    instructionsTitle.textContent = "Instructions";
+                    recipeCardEl.appendChild(instructionsTitle);
+                    instructionsTitle.textContent = data.drinks[0].strInstructions;
+                })
+        })
+    }
+}
